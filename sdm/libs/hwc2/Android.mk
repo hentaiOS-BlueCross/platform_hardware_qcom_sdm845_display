c9ac7297fa067dec90774014575b872eee590f19
Steven Moreland <smoreland@google.com>
2019-10-01 11:03:23 -0700

    Remove libhwbinder/libhidltransport deps
    Since these were combined into libhidlbase.
    
    Bug: 135686713
    Test: build only (libhwbinder/libhidltransport are empty)
    Change-Id: I703fb23d9bd4453f6e50d3a8c0244fca569b4c1d

--- sdm/libs/hwc2/Android.mk
+++ sdm/libs/hwc2/Android.mk
@@ -19,7 +19,7 @@ LOCAL_CLANG                   := true
 LOCAL_SHARED_LIBRARIES        := libsdmcore libqservice libbinder libhardware libhardware_legacy \
                                  libutils libcutils libsync libqdutils libqdMetaData \
                                  libsdmutils libc++ liblog libgrallocutils libui libgpu_tonemapper \
-                                 libhidlbase libhidltransport vendor.display.config@1.0 \
+                                 libhidlbase vendor.display.config@1.0 \
                                  android.hardware.graphics.mapper@2.0 \
                                  android.hardware.graphics.mapper@2.1 \
                                  android.hardware.graphics.composer@2.2 \
